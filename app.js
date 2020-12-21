7a7bf82820934598b50bf282ff559c60c6a3dd0e
Rupa Dhillon <rupa211984@gmail.com>
2020-12-20 17:21:36 -0800

    Add env and update message array

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const server = require("http").createServer(app);
-const socketPort = process.env.PORT || 8000;
+const socketPort = process.env.PORT;
 const db = require("./controller");
 const io = require("socket.io")(server, {
   cors: {
-    origin: "https://chattermeow.netlify.app",
+    origin: process.env.ALLOWED_ORIGIN,
     methods: ["GET", "POST"],
   },
 });
@@ -26,16 +26,18 @@ db.getSocketMessages()
   })
   .catch(console.log);
 
-// connects, creates message, and emits top 10 messages
+// connects, creates message, and emits top 20 messages
 io.on("connection", (socket) => {
   console.log("a user connected");
 
   socket.on("chat message", (msg) => {
     db.createSocketMessage(JSON.parse(msg))
       .then((msg) => {
-        recentMessages.pop();
+        if (recentMessages.length >= 20) {
+          recentMessages.pop();
+        }
+
         recentMessages.unshift(msg);
-        console.log(recentMessages);
         io.emit("chat message", recentMessages);
       })
       .catch((err) => io.emit(err));
